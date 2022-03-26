badc381bb445bc3fb463ed59abc996a9f1e37471
brillience <360727078@qq.com>
2022-03-26 15:30:15 +0800

    fix update PDF to db bug

--- runSpider.py
+++ runSpider.py
@@ -4,4 +4,34 @@
 # @Author : ZhangXiaobo
 # @Software: PyCharm
 from scrapy import cmdline
-cmdline.execute('scrapy crawl wos_spider'.split())
+from wos_crawl.settings import FILES_STORE,DB_PATH
+from wos_crawl.model.models import CrawlerStates
+from wos_crawl.model import get_engine,get_session
+import os
+
+def updateToDb():
+    pdf_list = os.listdir(FILES_STORE)
+    engine = get_engine(db_url=DB_PATH)
+    session = get_session(engine)
+
+    # 将爬取成功的文献更新的数据库
+    for pdf in pdf_list:
+        file_name = pdf.split('.')[0]
+        print(file_name)
+        paper = session.query(CrawlerStates).filter(CrawlerStates.uniqueid == file_name).first()
+        if paper.state ==False:
+            paper.state=True
+            session.commit()
+
+    session.close()
+
+
+
+while True:
+    beforePdfSize = len(os.listdir(FILES_STORE))
+    cmdline.execute('scrapy crawl wos_spider'.split())
+    pdf_list = os.listdir(FILES_STORE)
+    updateToDb()
+    if len(pdf_list)<=beforePdfSize:
+        print("FINISHED GETTING PDF FILES!")
+        break
--- wos_crawl/updateFilesToDb.py
+++ wos_crawl/updateFilesToDb.py
@@ -1,13 +1,13 @@
 import os
 
 from model.models import CrawlerStates
-from settings import FILES_STORE
+from settings import FILES_STORE,DB_PATH
 from model import get_engine,get_session
 
 if __name__ == '__main__':
 
     pdf_list = os.listdir(FILES_STORE)
-    engine = get_engine()
+    engine = get_engine(DB_PATH)
     session = get_session(engine)
 
     # 将爬取成功的文献更新的数据库
